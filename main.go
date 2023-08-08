17aed5d6eaf0f7084cd3104059c44d22f253b3c7
xp <pengx@mail.ustc.edu.cn>
2023-08-08 23:51:11 +0800

    update the first leetcode test code!

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"leetcode/test"
+)
+
+func main() {
+	fmt.Println(test.Add(1, 2))
+	fmt.Println(test.Sub(2, 3))
+	fmt.Println(test.Mul(3, 4))
+	fmt.Println(test.Div(2, 4))
+}
---
+++ test/test.go
@@ -0,0 +1,17 @@
+package test
+
+func Add(a, b int) int {
+	return a + b
+}
+
+func Sub(a, b int) int {
+	return a - b
+}
+
+func Mul(a, b int) int {
+	return a * b
+}
+
+func Div(a, b int) float64 {
+	return float64(a) / float64(b)
+}
